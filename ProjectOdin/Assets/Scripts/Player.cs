1bb46fe0301a789660b404d9f293e02a78c3e484
Adam <imposter110@gmail.com>
2015-11-04 12:58:36 -0800

    Basic matchmaking should work. can walk around and look at each other and its not too janky

---
+++ ProjectOdin/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public float speed;
+    Vector2 rot;
+
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 move1 = transform.forward * v + transform.right * h;
+        move1.Normalize();
+        move1 *= speed;
+
+        move1.y = GetComponent<Rigidbody>().velocity.y;
+
+        GetComponent<Rigidbody>().velocity = move1;
+        
+
+        rot = new Vector2(
+        rot.x + Input.GetAxis("Mouse X") * 3,
+        rot.y + Input.GetAxis("Mouse Y") * 3);
+
+        transform.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
+    }
+}
--- ProjectOdin/Assets/Scripts/PlayerNetworkSetup.cs
+++ ProjectOdin/Assets/Scripts/PlayerNetworkSetup.cs
@@ -4,14 +4,14 @@ using UnityEngine.Networking;
 
 public class PlayerNetworkSetup : NetworkBehaviour {
 
-    [SerializeField] GameObject myCam;
+    [SerializeField] Camera myCam;
 
 	// Use this for initialization
 	void Start () {
 	    if(isLocalPlayer)
         {
-            GetComponent<CamMove>().enabled = true;
-            myCam.SetActive(true);
+            GetComponent<Player>().enabled = true;
+            myCam.enabled = true;
         }
 	}
 }
