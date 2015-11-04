837d12ce08ad31d5ac5a7cf7a8fce8f81f9ba129
Adam <imposter110@gmail.com>
2015-11-03 18:45:56 -0800

    fixed seeing everything

---
+++ ProjectOdin/Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+    public GameObject myCam;
+
+	// Use this for initialization
+	void Start () {
+	    if(isLocalPlayer)
+        {
+            GetComponent<CamMove>().enabled = true;
+            myCam.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
