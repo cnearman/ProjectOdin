c8ab197a9389cb490e6a30100a064877378497d0
Chris <nearman.cj@gmail.com>
2015-12-13 07:50:14 -0800

    Made shooting happen.

--- ProjectOdin/Assets/Scripts/Entities/Weapons/Gun.cs
+++ ProjectOdin/Assets/Scripts/Entities/Weapons/Gun.cs
@@ -9,14 +9,13 @@ public enum GunPriority
 
 public class Gun : BaseClass, IWeapon
 {
-    public GameObject Projectile;
-
     public GameObject Distribution;
 
     public GameObject Owner;
 
     private float RefireTime;
     public float FireRate;
+    public string Projectile;
 
     public GunPriority Priority;
 
@@ -38,14 +37,14 @@ public class Gun : BaseClass, IWeapon
 
     public void Fire()
     {
-        Instantiate(Projectile, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+        PhotonNetwork.Instantiate(Projectile, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0), 0);
     }
 
     public void Fire(Vector3 startingPosition, Quaternion direction)
     {
         if (CanFire)
         {
-            Instantiate(Projectile, startingPosition, direction);
+            PhotonNetwork.Instantiate(Projectile, startingPosition, direction, 0);
             AddCoolDown(FireRate);
         }
     }
--- ProjectOdin/Assets/Scripts/Entities/Weapons/Projectiles/ProjectileComponents/ForwardMotion.cs
+++ ProjectOdin/Assets/Scripts/Entities/Weapons/Projectiles/ProjectileComponents/ForwardMotion.cs
@@ -4,9 +4,19 @@ public class ForwardMotion : BaseClass, IProjectileMotion
 {
     public float Velocity;
 
+    private PhotonView m_PhotonView;
+
+    void Start()
+    {
+        m_PhotonView = GetComponent<PhotonView>();
+    }
+
     void Update() 
     {
-        transform.Translate(Vector3.forward * 1.0f * Time.deltaTime * Velocity);
+        if (m_PhotonView.isMine)
+        {
+            transform.Translate(Vector3.forward * 1.0f * Time.deltaTime * Velocity);
+        }
     }
 
 }
